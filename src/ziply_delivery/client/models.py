677f6c99e3aeb93c29b218e3b59550caa328679d
shar-hylton <shar.hyl10@gmail.com>
2025-11-29 16:23:38 -0500

    models completed

--- src/ziply_delivery/client/models.py
+++ src/ziply_delivery/client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    telephone = models.CharField(max_length=20)
+    account = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.first_name}{self.last_name}"
